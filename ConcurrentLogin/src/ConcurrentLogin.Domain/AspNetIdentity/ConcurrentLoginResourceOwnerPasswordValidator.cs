2533c0b991cd4361c5d9adb9c0b38f49cd88a43e
Raul7Aliyev <raraul5676@gmail.com>
2023-05-26 12:30:47 +0900

    This is a completed version

---
+++ ConcurrentLogin/src/ConcurrentLogin.Domain/AspNetIdentity/ConcurrentLoginResourceOwnerPasswordValidator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using IdentityServer4.AspNetIdentity;
+using IdentityServer4.Validation;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Identity;
+using Volo.Abp.IdentityServer.AspNetIdentity;
+using Volo.Abp.IdentityServer.Localization;
+using IdentityUser = Volo.Abp.Identity.IdentityUser;
+
+namespace ConcurrentLogin.AspNetIdentity;
+
+public class ConcurrentLoginResourceOwnerPasswordValidator : AbpResourceOwnerPasswordValidator
+{
+
+    public ConcurrentLoginResourceOwnerPasswordValidator(
+        UserManager<IdentityUser> userManager,
+        SignInManager<IdentityUser> signInManager,
+        IdentitySecurityLogManager identitySecurityLogManager,
+        ILogger<ResourceOwnerPasswordValidator<IdentityUser>> logger,
+        IStringLocalizer<AbpIdentityServerResource> localizer,
+        IOptions<AbpIdentityOptions> abpIdentityOptions,
+        IServiceScopeFactory serviceScopeFactory,
+        IOptions<IdentityOptions> identityOptions) : base(userManager, signInManager, identitySecurityLogManager, logger, localizer, abpIdentityOptions, serviceScopeFactory, identityOptions)
+    {
+    }
+
+    protected override async Task SetSuccessResultAsync(ResourceOwnerPasswordValidationContext context, IdentityUser user)
+    {
+        user.SetProperty(ConcurrentLoginConsts.ConcurrentLoginToken, Guid.NewGuid().ToString("N"));
+        await UserManager.UpdateAsync(user);
+         await base.SetSuccessResultAsync(context, user);
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# ABP Framework Sample Projects
+
+Sample solutions built with the ABP Framework.
+
+### Issues
+
+Please create issues to this main repository
+
+### Pull Requests
+
+You can send PRs to this repository.
